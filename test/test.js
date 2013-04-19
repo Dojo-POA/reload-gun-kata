067b426ed3b539cdba2f84446433ac7cf42bbe23
Bruno Tavares <bruno.exz@gmail.com>
2013-04-19 18:32:16 -0300

    Dojo Apr 18 - First part of the problem

--- test/test.js
+++ test/test.js
@@ -1,10 +1,27 @@
 var expect = require("chai").expect;
 var sinon = require("sinon");
 
-describe('bla', function() {
+var Gun = require('../lib/gun').Gun;
 
-  it('bla', function(){
-    expect(1).to.equal(1);
+describe('Gun', function() {
+  describe('when shooting', function() {
+    it('should make BANG', function(){
+      var gun = new Gun();
+
+      expect(gun.fire()).to.equal("BANG");
+    });
+    
+    it('should make CLICK when there are no bullets', function(){
+      var gun = new Gun();
+      shootFiveTimes(gun);
+      expect(gun.fire()).to.equal("CLICK");
+    });     
+    
+    var shootFiveTimes = function(gun){
+      var i;
+      for (i = 0; i < gun.MAX_BULLETS; i++){
+        gun.fire();
+      }
+    }
   });
-
 });
