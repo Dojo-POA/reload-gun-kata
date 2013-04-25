f9096088609be0cd2383e6e25bfd2323faa92031
Bruno Tavares <bruno.exz@gmail.com>
2013-04-25 20:56:43 -0300

    Segundo DOJO

--- test/test.js
+++ test/test.js
@@ -1,27 +1,40 @@
 var expect = require("chai").expect;
 var sinon = require("sinon");
-
 var Gun = require('../lib/gun').Gun;
 
-describe('Gun', function() {
-  describe('when shooting', function() {
-    it('should make BANG', function(){
+describe('A gun', function(){
+  describe('by default', function(){
+    it("should have 5 bullets", function() {
       var gun = new Gun();
-
-      expect(gun.fire()).to.equal("BANG");
+      expect(gun.numberOfBullets).to.equal(5);
     });
-    
-    it('should make CLICK when there are no bullets', function(){
-      var gun = new Gun();
-      shootFiveTimes(gun);
-      expect(gun.fire()).to.equal("CLICK");
-    });     
-    
-    var shootFiveTimes = function(gun){
-      var i;
-      for (i = 0; i < gun.MAX_BULLETS; i++){
-        gun.fire();
-      }
-    }
+
+    it("should not have more than 5 bullets", function() {
+        var gun = new Gun(6);
+        expect(gun.numberOfBullets).to.equal(5);
+    });
   });
+  
+  describe('when shoting', function(){
+    it("should decreases the number of bullets", function() {
+      var gun = new Gun(1);
+      gun.fire();
+      expect(gun.numberOfBullets).to.equal(0);
+    });    
+  });
+
+})
+
+describe('A Gun with bullets', function () {
+  var gun = new Gun();
+    it('should make BANG while firing', function () {
+      expect(gun.fire()).to.equal("BANG");
+    });      
+});
+
+describe('A Gun without bullets', function () {  
+    it('should make CLICK while firing', function () {
+      var gun = new Gun(0);
+      expect(gun.fire()).to.equal("CLICK");
+    });
 });
